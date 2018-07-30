a0a8a7594168be556139c2e2324c9b164e5cca93
timfung93 <timothy.fung.04@gmail.com>
2018-07-29 23:24:33 -0400

    deleted unused phx files

--- lib/wiki_node_nav_web/controllers/page_controller.ex
+++ lib/wiki_node_nav_web/controllers/page_controller.ex
@@ -2,9 +2,5 @@ defmodule WikiNodeNavWeb.PageController do
   use WikiNodeNavWeb, :controller
   alias Wiki
 
-  def index(conn, _params) do
-    render conn, "index.html"
-  end
-
   def post(conn, params), do: Wiki.get_links(params)
 end
--- lib/wiki_node_nav_web/views/layout_view.ex
+++
@@ -1,3 +0,0 @@
-defmodule WikiNodeNavWeb.LayoutView do
-  use WikiNodeNavWeb, :view
-end
