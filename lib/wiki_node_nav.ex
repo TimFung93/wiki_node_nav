c549c43dacf6225d3b79ddbf65e5dcda734d31b7
timfung93 <timothy.fung.04@gmail.com>
2018-08-05 00:19:24 -0400

    wip

--- lib/wiki_node_nav.ex
+++ lib/wiki_node_nav.ex
@@ -1,6 +1,7 @@
 defmodule Wiki do
   require IEx
   @url_base_link "https://en.wikipedia.org/wiki/"
+  @url "https://en.wikipedia.org"
   @wiki_black_list ~w(/wiki/File: /wiki/Category: /wiki/Special: /wiki/Wikipedia: /wiki/Portal: /wiki/Talk: /wiki/Help: /wiki/Main_Page)
   @link_word_map %{link: []}
 
@@ -12,6 +13,9 @@ defmodule Wiki do
     last_word =
       HTTPoison.get(@url_base_link <> "#{params["last_word"]}")
       |> process_response_body()
+
+    # IEx.pry
+
 
     compare(start_word, last_word)
   end
@@ -49,17 +53,51 @@ defmodule Wiki do
       end)
       |> List.flatten()
       |> Enum.filter(& &1)
-    build_link(@link_word_map.link ++ similar_links_list)
+
+    dispatch_to_task(@link_word_map.link ++ similar_links_list)
   end
 
-  def build_link(list_of_items \\ []) do
-    Enum.each(list_of_items, fn(item) ->
-      test = spawn fn -> HTTPoison.get(@url_base_link <> "#{item}")
+  def build_link(item) do
 
+    slice_string = String.slice(item, 1..-1)
+
+    get_request = HTTPoison.get(@url <> "#{slice_string}")
+
+
+
+    # target = self()
+
+    # spawn(fn ->
+    #   # write stuff to disk
+    #   # takes long time
+
+    # # send a message to it
+    #   send(target, result_of_write)
+
+    # end)
+
+    # dispatch_to_task(list_of_items)
+    # target = self()
+    #  spawn(fn ->
+    #   link = Enum.reduce(list_of_items, %{}, fn(item, acc) ->
+    #     get_request = HTTPoison.get(@url_base_link <> "#{item}")
+
+    #     IEx.pry
+
+    #   end)
+
+    #   send(target, link)
+    # end)
       end
 
-      IEx.pry
-    end)
+  defp dispatch_to_task(list_of_items \\ []) do
+    test =
+      list_of_items
+      |> Enum.map(&Task.async(fn -> build_link(&1) end))
+      |> Enum.map(&Task.await(&1))
+
+    IEx.pry
+
   end
 
 end
--- lib/wiki_node_nav_web/controllers/page_controller.ex
+++ lib/wiki_node_nav_web/controllers/page_controller.ex
@@ -2,5 +2,14 @@ defmodule WikiNodeNavWeb.PageController do
   use WikiNodeNavWeb, :controller
   alias Wiki
 
-  def post(conn, params), do: Wiki.get_links(params)
+  def post(conn, params) do
+    Wiki.get_links(params)
+
+    receive do
+      result ->
+        require IEx
+        IEx.pry
+    end
+
+  end
 end
